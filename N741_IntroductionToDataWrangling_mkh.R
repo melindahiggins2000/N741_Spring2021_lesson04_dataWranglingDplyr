3619f2c226e5bbf3bb56ffa130e42ae8bbd4f522
melindahiggins2002 <mhiggins2002@aol.com>
2021-02-17 11:21:24 -0500

    final updates to dplyr slides and cleaned R scripts

--- N741_IntroductionToDataWrangling_mkh.R
+++ N741_IntroductionToDataWrangling_mkh.R
@@ -1,65 +1,44 @@
-## ----setup, include=FALSE------------------------------
-# For htmltools package, you may set options(htmltools.dir.version = FALSE) to suppress the version number in the subdirectory name.
-options(htmltools.dir.version = FALSE)
 library(knitr)
 library(dplyr)
 
-
-## ------------------------------------------------------
 library(palmerpenguins)
 str(penguins)
 
-
-## ------------------------------------------------------
 head(penguins)
 
-
-## ------------------------------------------------------
 class(penguins)
 
-
-## ------------------------------------------------------
 tail(penguins, 10)
 
-
-## ----echo=FALSE----------------------------------------
-a <- getOption("width")
-options(width=120)
-
-
-## ------------------------------------------------------
 penguins.df <- data.frame(penguins)
 class(penguins.df)
 head(penguins.df)
 
-
-## ----echo=FALSE----------------------------------------
-options(width=a)
-
-
-## ------------------------------------------------------
 penguins %>%
   pull(bill_length_mm) %>%
   mean()
 
 
-## ------------------------------------------------------
 penguins %>%
   pull(bill_length_mm) %>%
   mean(na.rm = TRUE)
 
-
-## ------------------------------------------------------
 mean(penguins$bill_length_mm, na.rm = TRUE)
 
-
-## ------------------------------------------------------
 penguins %>%
   select(body_mass_g, flipper_length_mm) %>%
   summary()
 
+library(Hmisc)
+penguins %>%
+  select(body_mass_g, flipper_length_mm) %>%
+  Hmisc::describe()
 
-## ------------------------------------------------------
+library(psych)
+penguins %>%
+  select(body_mass_g, flipper_length_mm) %>%
+  psych::describe()
+
 penguins %>%
   select(body_mass_g, flipper_length_mm) %>%
   summarise(
@@ -69,8 +48,6 @@ penguins %>%
     flip_sd = sd(flipper_length_mm, na.rm=TRUE)
   )
 
-
-## ------------------------------------------------------
 penguins %>%
   select(body_mass_g, flipper_length_mm, species) %>%
   group_by(species) %>% #<<
@@ -81,8 +58,6 @@ penguins %>%
     flip_sd = sd(flipper_length_mm, na.rm=TRUE)
   )
 
-
-## ------------------------------------------------------
 penguins %>%
   select(body_mass_g, flipper_length_mm, species) %>%
   group_by(species) %>%
@@ -94,8 +69,6 @@ penguins %>%
   ) %>%
   knitr::kable() #<<
 
-
-## ------------------------------------------------------
 penguins %>%
   select(body_mass_g, flipper_length_mm, species) %>%
   group_by(species) %>%
@@ -111,76 +84,44 @@ penguins %>%
                              "Mean Flipper Length (mm)", #<<
                              "SD of Flipper Length (mm)")) #<<
 
-
-## ----out.width = '=100%', echo=FALSE-------------------
-knitr::include_graphics("img/dplyr_across.png")
-
-
-## ------------------------------------------------------
 penguins %>%
-  group_by(species) %>%
-  summarize(across(ends_with("mm"), mean, na.rm = TRUE)) %>% #<<
+  dplyr::group_by(species) %>%
+  dplyr::summarize(across(ends_with("mm"), mean, na.rm = TRUE)) %>% #<<
   knitr::kable()
 
-
-## ----out.width = '=100%', echo=FALSE-------------------
-knitr::include_graphics("img/dplyr_filter.jpg")
-
-
-## ----echo=FALSE----------------------------------------
-a <- getOption("width")
-options(width=120)
-
-
-## ------------------------------------------------------
 penguins %>%
   filter(species == "Chinstrap") %>% #<<
   summary()
 
-
-## ----echo=FALSE----------------------------------------
-options(width=a)
-
-
-## ----out.width = '75%', echo=FALSE---------------------
-knitr::include_graphics("img/dplyr_mutate.png")
-
-
-## ------------------------------------------------------
 penguins.mod <- penguins %>%
   mutate(body_mass_kg = body_mass_g / 1000)
 
 penguins.mod %>%
   select(body_mass_g, body_mass_kg) %>%
   head()
+
+# Use case_when() to create
+# three size categories
+penguins_mod <- penguins %>%
+  mutate(size_bin = case_when(
+    body_mass_g > 4500 ~ "large",
+    body_mass_g > 3000 & 
+      body_mass_g <= 4500 ~ "medium",
+    body_mass_g <= 3000 ~ "small"
+  )
+  )
+
+# Create summary table 
+# of size categories
+penguins_mod %>% 
+  pull(size_bin) %>% 
+  table(useNA = "ifany") %>% #<<
+  knitr::kable(
+    col.names = c("Size Category",
+                  "Frequency"))
   
-
-
-## ----out.width = '75%', echo=FALSE---------------------
-knitr::include_graphics("img/dplyr_case_when.png")
-
-
-## ----tablesize, eval=FALSE-----------------------------
-## # Use case_when() to create
-## # three size categories
-## penguins_mod <- penguins %>%
-##   mutate(size_bin = case_when(
-##     body_mass_g > 4500 ~ "large",
-##     body_mass_g > 3000 &
-##       body_mass_g <= 4500 ~ "medium",
-##     body_mass_g <= 3000 ~ "small"
-##     )
-##   )
-## 
-## # Create summary table
-## # of size categories
-## penguins_mod %>%
-##   pull(size_bin) %>%
-##   table(useNA = "ifany") %>% #<<
-##   knitr::kable(
-##     col.names = c("Size Category",
-##                   "Frequency"))
-
-
-## ----tablesize-out, ref.label="tablesize", echo=FALSE----
+library(arsenal)
+tab1 <- tableby(island ~ bill_length_mm + bill_depth_mm, 
+                data = penguins)
+summary(tab1)
 
--- N741_IntroductionToGgplot2_mkh.R
+++ N741_IntroductionToGgplot2_mkh.R
@@ -1,257 +1,147 @@
-## ----setup, include=FALSE-------------------------------------
-# For htmltools package, you may set options(htmltools.dir.version = FALSE) to suppress the version number in the subdirectory name.
-options(htmltools.dir.version = FALSE)
-knitr::opts_chunk$set(echo = TRUE)
-knitr::opts_chunk$set(warning = FALSE)
-knitr::opts_chunk$set(message = FALSE)
-
 library(knitr)
 library(dplyr)
 library(ggplot2)
 
-
-## ----out.width = '80%', echo=FALSE----------------------------
-knitr::include_graphics("img/ggfree.jpg")
-
-
-## ----out.width = '75%', echo=FALSE----------------------------
-knitr::include_graphics("img/mancalas.jpg")
-
-
-## ----message=FALSE, warning=FALSE, error=FALSE----------------
 library(kandinsky)
 kandinsky(iris)
-
-
-## -------------------------------------------------------------
 kandinsky(airquality)
 
+library(palmerpenguins)
+ 
+# Make a scatterplot
+ggplot(penguins) +
+  aes(x=bill_length_mm,
+     y=bill_depth_mm,
+     color = species) +
+  geom_point() +
+  xlab("Bill Length (mm)") +
+  ylab("Bill Depth (mm)") +
+  ggtitle("Penguins Bill Dimensions")
 
-## ----out.width = '100%', echo=FALSE---------------------------
-knitr::include_graphics("img/rgraphgallery_parallel.JPG")
+# Step 1 specify dataset
+# penguins for ggplot()
+ggplot(penguins) 
 
+# Next add aes (aesthetics)
+ggplot(penguins) +
+ aes(x=bill_length_mm, #<<
+     y=bill_depth_mm)  #<<
 
-## ----out.width = '100%', echo=FALSE---------------------------
-knitr::include_graphics("img/rgraphgallery_boxplotjitter.JPG")
+# Add points to graph
+ggplot(penguins) +
+ aes(x=bill_length_mm,
+     y=bill_depth_mm) +
+ geom_point() #<<
+ 
+# Add color aesthetic
+ggplot(penguins) +
+ aes(x=bill_length_mm,
+     y=bill_depth_mm,
+     color = species) + #<<
+ geom_point()
 
+# Add axis labels and a title
+ggplot(penguins) +
+ aes(x=bill_length_mm,
+     y=bill_depth_mm,
+     color = species) +
+ geom_point() +
+ xlab("Bill Length(mm)") +   #<<
+ ylab("Bill Depth (mm)") +    #<<
+ ggtitle("Penguins Bill Dimensions")  #<<
 
-## ----out.width = '80%', echo=FALSE----------------------------
-knitr::include_graphics("img/rgraphgallery_violinboxplot.JPG")
+# Add axis labels and a title
+ggplot(penguins) +
+ aes(x=bill_length_mm,
+     y=bill_depth_mm,
+     color = species) +
+ geom_point() +
+ xlab("Bill Length(mm)") +
+ ylab("Bill Depth (mm)") +
+ ggtitle("Penguins Bill Dimensions") +
+ theme_dark() #<<
 
+library(ggthemes)
+ 
+ggplot(penguins) +
+ aes(x=bill_length_mm,
+     y=bill_depth_mm,
+     color = species) +
+ geom_point() +
+ xlab("Bill Length(mm)") +
+ ylab("Bill Depth (mm)") +
+ ggtitle("WSJ Theme") +
+ theme_wsj()
 
-## ----out.width = '100%', echo=FALSE---------------------------
-knitr::include_graphics("img/rgraphgallery_lollipoprose.JPG")
+library(ggthemr)
 
+ggthemr('pale')
+ggplot(penguins,
+      aes(x=bill_length_mm,
+          y=bill_depth_mm,
+          colour = species)) +
+ geom_point() +
+ xlab("Bill Length(mm)") +
+ ylab("Bill Depth (mm)") +
+ ggtitle("ggthemr - pale theme") +
+ scale_colour_ggthemr_d()
+ggthemr_reset()
+ 
+# Look at flipper_length_mm
+# for Palmer Penguins
+# Histogram with density curve
+# Use y=..density..
+# Overlay with transparent density plot
+ggplot(penguins,
+      aes(x=flipper_length_mm)) +
+ geom_histogram(aes(y=..density..),
+                binwidth=2,
+                colour="black",
+                fill="white") +
+ geom_density(alpha=.2,
+              fill="#FF6666")
 
-## ----plot-label, eval=FALSE-----------------------------------
-## library(palmerpenguins)
-## 
-## # Make a scatterplot
-## ggplot(penguins) +
-##   aes(x=bill_length_mm,
-##       y=bill_depth_mm,
-##       color = species) +
-##   geom_point() +
-##   xlab("Bill Length (mm)") +
-##   ylab("Bill Depth (mm)") +
-##   ggtitle("Penguins Bill Dimensions")
-## 
+# Look at flipper_length_mm
+# for Palmer Penguins
+# Histogram with density curve
+# Use y=..density..
+# Overlay with transparent density plot
+ggplot(penguins,
+      aes(x=flipper_length_mm)) +
+ geom_histogram(aes(y=..density..),
+                binwidth=2,
+                colour="black",
+                fill="white") +
+ geom_density(alpha=.2,
+              fill="#FF6666") +
+ facet_wrap(vars(species)) #<<
 
+library(ggplot2)
+library(patchwork)
 
-## ----plot-label-out, ref.label="plot-label", echo=FALSE, out.width="100%"----
+# save each plot as an object
+p1 <- ggplot(penguins,
+        aes(x=bill_length_mm,
+            y=bill_depth_mm,
+            color = species)) +
+ geom_point()
 
-
-## ----plot-label1, eval=FALSE----------------------------------
-## # Step 1 specify dataset
-## # penguins for ggplot()
-## ggplot(penguins) #<<
-
-
-## ----plot-label1-out, ref.label="plot-label1", echo=FALSE, out.width="100%"----
-
-
-## ----plot-label2, eval=FALSE----------------------------------
-## # Next add aes (aesthetics)
-## ggplot(penguins) +
-##   aes(x=bill_length_mm, #<<
-##       y=bill_depth_mm)  #<<
-
-
-## ----plot-label2-out, ref.label="plot-label2", echo=FALSE, out.width="100%"----
-
-
-## ----plot-label3, eval=FALSE----------------------------------
-## # Add points to graph
-## ggplot(penguins) +
-##   aes(x=bill_length_mm,
-##       y=bill_depth_mm) +
-##   geom_point() #<<
-## 
-
-
-## ----plot-label3-out, ref.label="plot-label3", echo=FALSE, out.width="100%"----
-
-
-## ----plot-label4, eval=FALSE----------------------------------
-## # Add color aesthetic
-## ggplot(penguins) +
-##   aes(x=bill_length_mm,
-##       y=bill_depth_mm,
-##       color = species) + #<<
-##   geom_point()
-## 
-
-
-## ----plot-label4-out, ref.label="plot-label4", echo=FALSE, out.width="100%"----
-
-
-## ----plot-label5, eval=FALSE----------------------------------
-## # Add axis labels and a title
-## ggplot(penguins) +
-##   aes(x=bill_length_mm,
-##       y=bill_depth_mm,
-##       color = species) +
-##   geom_point() +
-##   xlab("Bill Length(mm)") +   #<<
-##   ylab("Bill Depth (mm)") +    #<<
-##   ggtitle("Penguins Bill Dimensions")  #<<
-## 
-
-
-## ----plot-label5-out, ref.label="plot-label5", echo=FALSE, out.width="100%"----
-
-
-## ----plot-label6, eval=FALSE----------------------------------
-## # Add axis labels and a title
-## ggplot(penguins) +
-##   aes(x=bill_length_mm,
-##       y=bill_depth_mm,
-##       color = species) +
-##   geom_point() +
-##   xlab("Bill Length(mm)") +
-##   ylab("Bill Depth (mm)") +
-##   ggtitle("Penguins Bill Dimensions") +
-##   theme_dark() #<<
-## 
-
-
-## ----plot-label6-out, ref.label="plot-label6", echo=FALSE, out.width="100%"----
-
-
-## ----ggtheme1, eval=FALSE-------------------------------------
-## library(ggthemes)
-## 
-## ggplot(penguins) +
-##   aes(x=bill_length_mm,
-##       y=bill_depth_mm,
-##       color = species) +
-##   geom_point() +
-##   xlab("Bill Length(mm)") +
-##   ylab("Bill Depth (mm)") +
-##   ggtitle("WSJ Theme") +
-##   theme_wsj()
-
-
-## ----ggtheme1-out, ref.label="ggtheme1", echo=FALSE, out.width="90%"----
-
-
-## ----ggthemr1, eval=FALSE-------------------------------------
-## library(ggthemr)
-## 
-## ggthemr('pale')
-## ggplot(penguins,
-##        aes(x=bill_length_mm,
-##            y=bill_depth_mm,
-##            colour = species)) +
-##   geom_point() +
-##   xlab("Bill Length(mm)") +
-##   ylab("Bill Depth (mm)") +
-##   ggtitle("ggthemr - pale theme") +
-##   scale_colour_ggthemr_d()
-## ggthemr_reset()
-## 
-
-
-## ----ggthemr1-out, ref.label="ggthemr1", echo=FALSE, out.width="90%"----
-
-
-## ----histplot, eval=FALSE-------------------------------------
-## # Look at flipper_length_mm
-## # for Palmer Penguins
-## # Histogram with density curve
-## # Use y=..density..
-## # Overlay with transparent density plot
-## ggplot(penguins,
-##        aes(x=flipper_length_mm)) +
-##   geom_histogram(aes(y=..density..),
-##                  binwidth=2,
-##                  colour="black",
-##                  fill="white") +
-##   geom_density(alpha=.2,
-##                fill="#FF6666")
-
-
-## ----histplot-out, ref.label="histplot", echo=FALSE, out.width="90%"----
-
-
-## ----histplot2, eval=FALSE------------------------------------
-## # Look at flipper_length_mm
-## # for Palmer Penguins
-## # Histogram with density curve
-## # Use y=..density..
-## # Overlay with transparent density plot
-## ggplot(penguins,
-##        aes(x=flipper_length_mm)) +
-##   geom_histogram(aes(y=..density..),
-##                  binwidth=2,
-##                  colour="black",
-##                  fill="white") +
-##   geom_density(alpha=.2,
-##                fill="#FF6666") +
-##   facet_wrap(vars(species)) #<<
-
-
-## ----histplot2-out, ref.label="histplot2", echo=FALSE, fig.dim=c(5, 3), out.width="100%"----
-
-
-## ----out.width = '20%', echo=FALSE----------------------------
-knitr::include_graphics("img/patchworklogo.png")
-
-
-## ----out.width = '80%', echo=FALSE----------------------------
-knitr::include_graphics("img/patchwork_blank.png")
-
-
-## ----plot-patch, eval=FALSE-----------------------------------
-## library(ggplot2)
-## library(patchwork)
-## 
-## # save each plot as an object
-## p1 <- ggplot(penguins,
-##          aes(x=bill_length_mm,
-##              y=bill_depth_mm,
-##              color = species)) +
-##   geom_point()
-## 
-## p2 <- ggplot(penguins,
-##              aes(x=species,
-##                  y=bill_length_mm)) +
-##   geom_boxplot() +
-##   xlab("Species") +
-##   ylab("Bill Depth (mm)")
-## 
-## p3 <-
-##   ggplot(penguins,
-##          aes(x=flipper_length_mm)) +
-##   geom_histogram(aes(y=..density..),
-##     binwidth=2, colour="black",
-##     fill="white") +
-##   geom_density(alpha=.2, fill="#FF6666") +
-##   facet_wrap(vars(species))
-## 
-## # arrange plots as you like
-## (p1 | p2) / p3
-
-
-## ----plot-patch-out, ref.label="plot-patch", echo=FALSE, fig.dim=c(8, 8)----
+p2 <- ggplot(penguins,
+            aes(x=species,
+                y=bill_length_mm)) +
+ geom_boxplot() +
+ xlab("Species") +
+ ylab("Bill Depth (mm)")
+ 
+p3 <-
+ ggplot(penguins,
+        aes(x=flipper_length_mm)) +
+ geom_histogram(aes(y=..density..),
+   binwidth=2, colour="black",
+   fill="white") +
+ geom_density(alpha=.2, fill="#FF6666") +
+ facet_wrap(vars(species))
+ 
+# arrange plots as you like
+(p1 | p2) / p3
 
